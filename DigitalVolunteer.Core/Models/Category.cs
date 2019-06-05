96ba07eaadc353b2bffc72f8a955c2957c305aff
Ilya Dzyubin <iDzyubin@yahoo.com>
2019-06-05 16:28:22 +0300

    Initial commit

---
+++ DigitalVolunteer.Core/Models/Category.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace DigitalVolunteer.Core.Models
+{
+    public class Category
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+}
