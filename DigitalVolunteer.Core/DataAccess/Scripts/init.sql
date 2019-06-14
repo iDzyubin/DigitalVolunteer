99821d84d248901f06964240494a37256c395202
Ilya Dzyubin <iDzyubin@yahoo.com>
2019-06-14 20:58:11 +0300

    Добавлены таблицы для задач и аккаунтов

--- DigitalVolunteer.Core/DataAccess/Scripts/init.sql
+++ DigitalVolunteer.Core/DataAccess/Scripts/init.sql
@@ -14,4 +14,46 @@ CREATE TABLE dv.categories
 (
     id uuid NOT NULL PRIMARY KEY,
     name character varying UNIQUE NOT NULL
-)
+);
+
+CREATE TABLE dv.accounts
+(
+    id uuid NOT NULL PRIMARY KEY,
+    first_name character varying NOT NULL,
+    last_name character varying NOT NULL,
+    description character varying NOT NULL
+);
+
+CREATE TABLE dv.tasks
+(
+    id uuid NOT NULL PRIMARY KEY,
+    title character varying NOT NULL,
+    description character varying,
+    start_date date NOT NULL,
+    end_date date,
+    contact_phone character varying NOT NULL,
+    task_format integer NOT NULL,
+    has_push_notifications boolean NOT NULL,
+    is_only_for_executors boolean NOT NULL,
+	owner_id uuid NOT NULL,
+	executor_id uuid NOT NULL
+);
+
+CREATE TABLE dv.task_owners_executors
+(
+    task_id uuid NOT NULL,
+    owner_id uuid NOT NULL,
+    executor_id uuid NOT NULL,
+    CONSTRAINT task_owners_executors__executor_id__accounts__id FOREIGN KEY (executor_id)
+        REFERENCES dv.accounts (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION,
+    CONSTRAINT task_owners_executors__owner_id__accounts__id FOREIGN KEY (owner_id)
+        REFERENCES dv.accounts (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION,
+    CONSTRAINT task_owners_executors__task_id__tasks__id FOREIGN KEY (task_id)
+        REFERENCES dv.tasks (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+);
--- DigitalVolunteer.Core/DataModels/Enums.cs
+++ DigitalVolunteer.Core/DataModels/Enums.cs
@@ -7,4 +7,10 @@
         Confirmed = 2,
         Deleted = 3
     }
+
+    public enum TaskFormat
+    {
+        Freelance = 0,
+        UpWork = 1
+    }
 }
