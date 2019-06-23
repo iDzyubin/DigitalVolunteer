35685f7f8832e1b41042d3229db708c24eba8748
iDzyubin <iDzyubin@yahoo.com>
2019-06-23 17:33:37 +0300

    Мелкие правки

--- DigitalVolunteer.Core/Extensions/UserExtensions.cs
+++ DigitalVolunteer.Core/Extensions/UserExtensions.cs
@@ -10,8 +10,8 @@ namespace DigitalVolunteer.Core.Extensions
 
 
         public static string GetShortDescription( this User user )
-            => !String.IsNullOrEmpty( user.Description ) && user.Description.Length > 120
-                ? user.Description.Substring( 0, 120 ) + "..."
+            => !String.IsNullOrEmpty( user.Description ) && user.Description.Length > 250
+                ? user.Description.Substring( 0, 250 ) + "..."
                 : user.Description;
     }
 }
